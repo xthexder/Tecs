c1b6ce3c9c6aa4443b9eddee1d857d4fd92be476
Jacob Wirth <xthexder@users.noreply.github.com>
2020-11-11 22:48:24 -0800

    Add CI testing config

---
+++ ci-build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+format_valid=0
+echo -e "--- Running \033[33mclang-format check\033[0m :clipboard:"
+if ! ./extra/validate_format.py; then
+    echo "^^^ +++"]
+    echo -e "\033[31mclang-format validation failed\033[0m"
+    format_valid=1
+fi
+
+mkdir -p build
+echo -e "--- Running \033[33mcmake configure\033[0m :video_game:"
+if ! cmake -DCMAKE_BUILD_TYPE=Release -S . -B ./build -GNinja; then
+    echo "^^^ +++"]
+    echo "\033[31mCMake Configure failed\033[0m"
+    exit 1
+fi
+
+echo -e "--- Running \033[33mcmake build\033[0m :rocket:"
+if ! cmake --build ./build --config Release --target all; then
+    echo "^^^ +++"]
+    echo "\033[31mCMake Build failed\033[0m"
+    exit 1
+fi
+
+echo -e "+++ Running \033[33mtests\033[0m :camera_with_flash:"
+cd build/tests
+
+success=0
+for file in ./Tecs-tests ./Tecs-benchmark; do
+    "./$file"
+    result=$?
+    if [ $result -ne 0 ]; then
+        echo -e "\033[31mTest failed with response code: $result\033[0m"
+        success=$result
+    else
+        echo -e "\033[32mTest successful\033[0m"
+    fi
+done
+
+if [ $success -ne 0 ]; then
+    echo -e "\033[31mTest failures detected\033[0m"
+    exit $success
+fi
+if [ $format_valid -ne 0 ]; then
+    echo -e "\033[31mclang-format errors detected\033[0m"
+    echo -e "Run clang-format with ./extra/validate_format.py --fix"
+    exit $format_valid
+fi
+
---
+++ extra/validate_format.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import re
+import glob
+import sys
+import subprocess
+
+include_paths = ["src/", "tests/"]
+include_extenions = [".cc", ".hh", ".cpp", ".hpp"]
+version_pattern = re.compile("version ([0-9]+\.[0-9]+)\.[0-9]+")
+allowed_clangformat_versions = ["6.0", "10.0"]
+
+def run_clang_format(filepath, fix):
+    if fix:
+        modifytime_before = os.path.getmtime(filepath)
+        clangformat_output = subprocess.getoutput('clang-format -i "' + filepath + '"')
+        modifytime_after = os.path.getmtime(filepath)
+        if modifytime_before != modifytime_after:
+            print('Formatted file: ' + filepath)
+        return True
+    else:
+        clangformat_output = subprocess.getoutput('clang-format --output-replacements-xml "' + filepath + '"')
+        if '<replacement ' in clangformat_output:
+            print('Needs format: ' + filepath)
+            return False
+        else:
+            return True
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fix", action='store_true', help="Fix formatting instead of validating.")
+    args = parser.parse_args()
+
+    project_root = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+
+    try:
+        clangformat_version = subprocess.getoutput('clang-format --version').strip()
+        clangformat_version = version_pattern.search(clangformat_version).group(1);
+        version_allowed = False
+        for allowed_version in allowed_clangformat_versions:
+            if allowed_version in clangformat_version:
+                version_allowed = True
+                break
+        if not version_allowed:
+            print('clang-format version not supported: ' + clangformat_version, file=sys.stderr)
+            exit(1)
+    except OSError:
+        print('Failed to run clang-format', file=sys.stderr)
+        raise
+
+    validated = True
+    for base_path in include_paths:
+        for extension in include_extenions:
+            glob_path = os.path.join(project_root, base_path, '**/*' + extension)
+            for filepath in glob.iglob(glob_path, recursive=True):
+                if not run_clang_format(filepath, args.fix):
+                    validated = False
+
+    if not validated:
+        exit(1)
+
+if __name__ == '__main__':
+    main()
