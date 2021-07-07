7dd0ad8ffa33ab9e34625a58e23b1ab2a7e24fa8
Jacob Wirth <xthexder@users.noreply.github.com>
2021-07-07 01:17:40 -0700

    Fix AssertHas helper

--- tests/tests.hh
+++ tests/tests.hh
@@ -8,21 +8,21 @@ namespace testing {
     template<typename... Tn, typename LockType>
     static inline void AssertHas(LockType &lock, Tecs::Entity &e) {
         if (Tecs::contains<Transform, Tn...>()) {
-            Assert(lock.Has<Transform>(e), "Entity is missing a Transform component");
+            Assert(lock.template Has<Transform>(e), "Entity is missing a Transform component");
         } else {
-            Assert(!lock.Has<Transform>(e), "Entity should not have a Transform component");
+            Assert(!lock.template Has<Transform>(e), "Entity should not have a Transform component");
         }
         if (Tecs::contains<Renderable, Tn...>()) {
-            Assert(lock.Has<Renderable>(e), "Entity is missing a Renderable component");
+            Assert(lock.template Has<Renderable>(e), "Entity is missing a Renderable component");
         } else {
-            Assert(!lock.Has<Renderable>(e), "Entity should not have a Renderable component");
+            Assert(!lock.template Has<Renderable>(e), "Entity should not have a Renderable component");
         }
         if (Tecs::contains<Script, Tn...>()) {
-            Assert(lock.Has<Script>(e), "Entity is missing a Script component");
+            Assert(lock.template Has<Script>(e), "Entity is missing a Script component");
         } else {
-            Assert(!lock.Has<Script>(e), "Entity should not have a Script component");
+            Assert(!lock.template Has<Script>(e), "Entity should not have a Script component");
         }
-        Assert(lock.Has<Tn...>(e), "Entity is missing components component");
+        Assert(lock.template Has<Tn...>(e), "Entity is missing components component");
     }
 
     void TestReadLock(Tecs::Lock<ECS, Tecs::Read<Transform>> lock);
